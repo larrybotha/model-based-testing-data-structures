4b8af248a3a580574f814e0e61d9f62712fd8675
Larry Botha <larry@fixate.it>
2021-01-16 15:52:15 +0200

    add property to stack tests

--- src/02-stack/with-fast-check/index.test.ts
+++ src/02-stack/with-fast-check/index.test.ts
@@ -15,4 +15,23 @@ describe("stack with fast-check", () => {
       })
     );
   });
+
+  test("-> always reads from top of stack", () => {
+    fc.assert(
+      fc.property(fc.integer({ min: 0 }), fc.array(fc.json()), (size, xs) => {
+        const stack = stackFactory();
+        const boundXs = xs.slice(0, size);
+
+        boundXs.map((x) => stack.push(x));
+        boundXs
+          .slice()
+          .reverse()
+          .map((x) => {
+            expect(stack.pop()).toEqual(x);
+          });
+
+        expect(stack.size()).toEqual(0);
+      })
+    );
+  });
 });
