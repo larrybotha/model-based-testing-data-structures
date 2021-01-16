8bc6e75a5cac999ca8dd4e40c1ae4e02a69ff2d7
Larry Botha <larry@fixate.it>
2021-01-16 15:53:02 +0200

    add tests for circular queue

--- src/05-circular-queue/with-fast-check/commands.ts
+++ src/05-circular-queue/with-fast-check/commands.ts
@@ -4,22 +4,71 @@ import { CircularQueue } from "..";
 
 type CircularQueueCommand = Command<any, CircularQueue>;
 
-interface Model {
-  read: number;
+export interface Model {
   values: any[];
-  write: number;
+  writes: number;
+  size: number;
 }
 
+/**
+ * EnqueueCommand.
+ *
+ * If the number of unread writes is less than the size of the queue, appends the
+ * value to the end of the queue, else returns null
+ *
+ * @implements {CircularQueueCommand}
+ */
 class EnqueueCommand implements CircularQueueCommand {
   constructor(public value: any) {}
 
   check = () => true;
 
-  run = (model: any, real: CircularQueue) => {
-    real.enqueue(this.value);
+  run = (model: Model, real: CircularQueue) => {
+    let modelValue = null;
+
+    if (model.writes < model.size) {
+      model.values.push(this.value);
+      model.writes++;
+      modelValue = this.value;
+    }
+
+    const realValue = real.enqueue(this.value);
+
+    expect(realValue).toEqual(modelValue);
   };
+
+  toString = () => `enqueue(${this.value})`;
 }
 
-const commands = [];
+/**
+ * DequeueCommand.
+ *
+ * If there have been any writes, reads first value in queue, else returns null
+ *
+ * @implements {CircularQueueCommand}
+ */
+class DequeueCommand implements CircularQueueCommand {
+  check = () => true;
+
+  run = (model: Model, real: CircularQueue) => {
+    let modelValue = null;
+
+    if (model.writes > 0) {
+      modelValue = model.values.shift();
+      model.writes--;
+    }
+
+    const realValue = real.dequeue();
+
+    expect(realValue).toEqual(modelValue);
+  };
+
+  toString = () => `dequeue`;
+}
+
+const commands = [
+  fc.json().map((value) => new EnqueueCommand(value)),
+  fc.constant(new DequeueCommand()),
+];
 
 export { commands };
--- src/05-circular-queue/with-fast-check/index.test.ts
+++ src/05-circular-queue/with-fast-check/index.test.ts
@@ -2,17 +2,69 @@ import fc from "fast-check";
 
 import { circularQueueFactory } from "..";
 
-import { commands } from "./commands";
+import { commands, Model } from "./commands";
 
 describe("circular queue with fast-check", () => {
   test("-> model-based", () => {
     fc.assert(
-      fc.property(fc.commands(commands, 100), (cmds) => {
-        const queue = circularQueueFactory();
-        const sut = () => ({ model: {}, real: queue });
+      fc.property(
+        /**
+         * Array implementation requires upper bound on size of queue, otherwise
+         * we get heap memory allocation error
+         */
+        //fc.integer({ min: 0, max: 10 }),
+        fc.integer({ min: 0 }),
+        fc.commands(commands, 100),
+        (size, cmds) => {
+          const queue = circularQueueFactory(size);
+          const model: Model = { writes: 0, size, values: [] };
+          const sut = () => ({ model, real: queue });
 
-        fc.modelRun(sut, cmds);
+          fc.modelRun(sut, cmds);
+        }
+      )
+    );
+  });
+
+  test("-> always reads from beginning of queue", () => {
+    fc.assert(
+      fc.property(fc.integer({ min: 0 }), fc.array(fc.json()), (size, xs) => {
+        const queue = circularQueueFactory(size);
+        const boundXs = xs.slice(0, size);
+
+        boundXs.map((x) => queue.enqueue(x));
+        boundXs.map((x) => {
+          expect(queue.dequeue()).toEqual(x);
+        });
       })
+    );
+  });
+
+  test("-> only writes to next value if number of unread writes is less than size", () => {
+    fc.assert(
+      fc.property(
+        /**
+         * Array implementation requires upper bound on size of queue, otherwise
+         * we get heap memory allocation error
+         */
+        //fc.integer({ min: 0, max: 10 }),
+        fc.integer({ min: 0 }),
+        fc.array(fc.json()),
+        (size, xs) => {
+          const queue = circularQueueFactory(size);
+          let writes = 0;
+
+          xs.map((x) => {
+            const result = queue.enqueue(x);
+
+            if (result !== null) {
+              writes = writes + 1;
+            }
+          });
+
+          expect(writes).toBeLessThanOrEqual(size);
+        }
+      )
     );
   });
 });
