fc0445e9ec92cbf9e4c749cd19ca616c81bce3cd
Larry Botha <larry@fixate.it>
2021-01-17 10:32:59 +0200

    add array implementation of circular queue

--- src/05-circular-queue/index.ts
+++ src/05-circular-queue/index.ts
@@ -4,26 +4,13 @@ export interface CircularQueue<T = any> {
   size(): number;
 }
 
+export type CircularQueueFactory = <T = any>(
+  maxSize: number
+) => CircularQueue<T>;
+
 const circularQueueFactory = function circularQueueFactory<T = any>(
   maxSize: number
 ): CircularQueue<T> {
-  /**
-   * Array implementation crashes for arbitrary array sizes
-   *
-   * O(n)
-   */
-  //let collection: Array<T | null> = Array(maxSize).fill(null);
-  /**
-   * object implementation
-   *
-   * O(1)
-   */
-  //let collection: Record<number, T | null> = {};
-  /**
-   * Map implementation
-   *
-   * O(1)
-   */
   let collection = new Map<number, T>();
   let readPointer = 0;
   let writePointer = 0;
@@ -38,33 +25,8 @@ const circularQueueFactory = function circularQueueFactory<T = any>(
   function enqueue(value: T) {
     let result = null;
 
-    if (
-      /**
-       * Array implementation
-       */
-      //collection[writePointer] === null
-
-      /**
-       * object implementation
-       */
-      //maxSize > 0 &&
-      //!(writePointer in collection)
-
-      /**
-       * Map implementation
-       */
-      maxSize > 0 &&
-      !collection.has(writePointer)
-    ) {
+    if (maxSize > 0 && !collection.has(writePointer)) {
       result = value;
-
-      /**
-       * array and object implementation
-       */
-      //collection[writePointer] = value;
-      /**
-       * Map implementation
-       */
       collection.set(writePointer, value);
 
       writePointer = (writePointer + 1) % maxSize;
@@ -81,43 +43,8 @@ const circularQueueFactory = function circularQueueFactory<T = any>(
   function dequeue() {
     let result = null;
 
-    if (
-      maxSize > 0 &&
-      /**
-       * Array implementation
-       */
-      //collection[readPointer] !== null
-
-      /**
-       * object implementation
-       */
-      //readPointer in collection
-
-      /**
-       * Map implementation
-       */
-      collection.has(readPointer)
-    ) {
-      /**
-       * array and object implementation
-       */
-      //result = collection[readPointer];
-      /**
-       * Map implementation
-       */
+    if (maxSize > 0 && collection.has(readPointer)) {
       result = collection.get(readPointer);
-
-      /**
-       * array implementation
-       */
-      //collection[readPointer] = null;
-      /**
-       * object implementation
-       */
-      //delete collection[readPointer];
-      /**
-       * Map implementation
-       */
       collection.delete(readPointer);
 
       readPointer = (readPointer + 1) % maxSize;
@@ -135,18 +62,87 @@ const circularQueueFactory = function circularQueueFactory<T = any>(
     let result;
 
     switch (true) {
-      /**
-       * array implementation
-       */
-      //case readPointer === writePointer && collection[readPointer] !== null: {
-      /**
-       * object implementation
-       */
-      //case readPointer === writePointer && readPointer in collection: {
-      /**
-       * Map implementation
-       */
       case readPointer === writePointer && collection.has(readPointer): {
+        result = maxSize;
+        break;
+      }
+
+      case readPointer > writePointer: {
+        result = maxSize - readPointer + writePointer;
+        break;
+      }
+
+      default: {
+        result = writePointer - readPointer;
+      }
+    }
+
+    return result;
+  }
+
+  return {
+    dequeue,
+    enqueue,
+    size,
+  };
+};
+
+const circularQueueArrayFactory = function circularQueueFactory<T = any>(
+  maxSize: number
+): CircularQueue<T> {
+  /**
+   * O(n)
+   */
+  let collection: Array<T | null> = Array(maxSize).fill(null);
+  let readPointer = 0;
+  let writePointer = 0;
+
+  /**
+   * enqueue
+   *
+   * O(1)
+   *
+   * @param {T} value
+   */
+  function enqueue(value: T) {
+    let result = null;
+
+    if (collection[writePointer] === null) {
+      result = value;
+      collection[writePointer] = value;
+      writePointer = (writePointer + 1) % maxSize;
+    }
+
+    return result;
+  }
+
+  /**
+   * dequeue
+   *
+   * O(1)
+   */
+  function dequeue() {
+    let result = null;
+
+    if (maxSize > 0 && collection[readPointer] !== null) {
+      result = collection[readPointer];
+      collection[readPointer] = null;
+      readPointer = (readPointer + 1) % maxSize;
+    }
+
+    return result;
+  }
+
+  /**
+   * size
+   *
+   * O(1)
+   */
+  function size() {
+    let result;
+
+    switch (true) {
+      case readPointer === writePointer && collection[readPointer] !== null: {
         result = maxSize;
         break;
       }
@@ -259,4 +255,8 @@ const circularQueueObjectFactory = function circularQueueFactory<T = any>(
   };
 };
 
-export { circularQueueFactory, circularQueueObjectFactory };
+export {
+  circularQueueFactory,
+  circularQueueObjectFactory,
+  circularQueueArrayFactory,
+};
