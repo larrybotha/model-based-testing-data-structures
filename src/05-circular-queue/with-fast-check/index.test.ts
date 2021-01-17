f0caaca25f40448320e59ee336d939bbc9a8f90c
Larry Botha <larry@fixate.it>
2021-01-17 10:33:17 +0200

    use parameterised tests for different queue implementations

--- src/05-circular-queue/with-fast-check/index.test.ts
+++ src/05-circular-queue/with-fast-check/index.test.ts
@@ -1,70 +1,82 @@
 import fc from "fast-check";
 
-import { circularQueueFactory } from "..";
+import {
+  circularQueueFactory,
+  circularQueueArrayFactory,
+  circularQueueObjectFactory,
+} from "..";
 
 import { commands, Model } from "./commands";
 
-describe("circular queue with fast-check", () => {
-  test("-> model-based", () => {
-    fc.assert(
-      fc.property(
-        /**
-         * Array implementation requires upper bound on size of queue, otherwise
-         * we get heap memory allocation error
-         */
-        //fc.integer({ min: 0, max: 10 }),
-        fc.integer({ min: 0 }),
-        fc.commands(commands, 100),
-        (size, cmds) => {
-          const queue = circularQueueFactory(size);
-          const model: Model = { writes: 0, size, values: [] };
-          const sut = () => ({ model, real: queue });
+describe.each`
+  name        | factory
+  ${"Map"}    | ${circularQueueFactory}
+  ${"Array"}  | ${circularQueueArrayFactory}
+  ${"Object"} | ${circularQueueObjectFactory}
+`(
+  "-> $name implementation of circular queue with fast-check",
+  ({ factory, name }) => {
+    /**
+     * Array implementation requires upper bound on size of queue, otherwise
+     * we get heap memory allocation error
+     */
+    const maxSize = new RegExp(name, "i").test("array") ? 100 : undefined;
 
-          fc.modelRun(sut, cmds);
-        }
-      )
-    );
-  });
+    test("-> model-based", () => {
+      fc.assert(
+        fc.property(
+          fc.integer({ min: 0, max: maxSize }),
+          fc.commands(commands, 100),
+          (size, cmds) => {
+            const queue = factory(size);
+            const model: Model = { writes: 0, size, values: [] };
+            const sut = () => ({ model, real: queue });
 
-  test("-> always reads from beginning of queue", () => {
-    fc.assert(
-      fc.property(fc.integer({ min: 0 }), fc.array(fc.json()), (size, xs) => {
-        const queue = circularQueueFactory(size);
-        const boundXs = xs.slice(0, size);
+            fc.modelRun(sut, cmds);
+          }
+        )
+      );
+    });
 
-        boundXs.map((x) => queue.enqueue(x));
-        boundXs.map((x) => {
-          expect(queue.dequeue()).toEqual(x);
-        });
-      })
-    );
-  });
+    test("-> always reads from beginning of queue", () => {
+      fc.assert(
+        fc.property(
+          fc.integer({ min: 0, max: maxSize }),
+          fc.array(fc.json()),
+          (size, xs) => {
+            const queue = factory(size);
+            const boundXs = xs.slice(0, size);
 
-  test("-> only writes to next value if number of unread writes is less than size", () => {
-    fc.assert(
-      fc.property(
-        /**
-         * Array implementation requires upper bound on size of queue, otherwise
-         * we get heap memory allocation error
-         */
-        //fc.integer({ min: 0, max: 10 }),
-        fc.integer({ min: 0 }),
-        fc.array(fc.json()),
-        (size, xs) => {
-          const queue = circularQueueFactory(size);
-          let writes = 0;
+            boundXs.map((x) => queue.enqueue(x));
+            boundXs.map((x) => {
+              expect(queue.dequeue()).toEqual(x);
+            });
+          }
+        )
+      );
+    });
 
-          xs.map((x) => {
-            const result = queue.enqueue(x);
+    test("-> only writes to next value if number of unread writes is less than size", () => {
+      fc.assert(
+        fc.property(
+          fc.integer({ min: 0, max: maxSize }),
+          fc.array(fc.json()),
+          (size, xs) => {
+            const queue = factory(size);
+            let writes = 0;
 
-            if (result !== null) {
-              writes = writes + 1;
-            }
-          });
+            xs.map((x) => {
+              const result = queue.enqueue(x);
 
-          expect(writes).toBeLessThanOrEqual(size);
-        }
-      )
-    );
-  });
-});
+              if (result !== null) {
+                writes = writes + 1;
+              }
+            });
+
+            expect(writes).toBeLessThanOrEqual(size);
+          }
+        )
+      );
+    });
+  }
+);
