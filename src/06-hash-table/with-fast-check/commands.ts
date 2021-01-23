099c0e74a1e0e9edf172dcd9293f41f7ffe4a015
Larry Botha <larry@fixate.it>
2021-01-23 23:28:16 +0200

    add hash table tests

--- src/06-hash-table/with-fast-check/commands.ts
+++ src/06-hash-table/with-fast-check/commands.ts
@@ -4,24 +4,93 @@ import { HashTable } from "..";
 
 type HashTableCommand = Command<any, HashTable>;
 
-export interface Model {}
+export interface Model {
+  [key: string]: any;
+}
 
 /**
- * []
- *
+ * AddCommand
  *
  * @implements {HashTableCommand}
  */
-class EnqueueCommand implements HashTableCommand {}
+class AddCommand implements HashTableCommand {
+  constructor(public key: string, public value: any) {}
+
+  check = () => true;
+
+  run = (m: Model, r: HashTable) => {
+    m[this.key] = this.value;
+
+    r.add(this.key, this.value);
+  };
+
+  toString = () => `add(${this.key}, ${this.value})`;
+}
 
 /**
- * []
- *
+ * RemoveCommand
  *
  * @implements {HashTableCommand}
  */
-class DequeueCommand implements HashTableCommand {}
+class RemoveCommand implements HashTableCommand {
+  constructor(private key: string) {}
 
-const commands = [];
+  check = () => true;
+
+  run = (m: Model, r: HashTable) => {
+    delete m[this.key];
+
+    r.remove(this.key);
+  };
+
+  toString = () => `remove(${this.key})`;
+}
+
+/**
+ * LookupCommand
+ *
+ * @implements {HashTableCommand}
+ */
+class LookupCommand implements HashTableCommand {
+  constructor(private key: string) {}
+
+  check = () => true;
+
+  run = (m: Model, r: HashTable) => {
+    expect(r.lookup(this.key)).toEqual(m[this.key]);
+  };
+
+  toString = () => `lookup(${this.key})`;
+}
+
+const tuples = fc.sample(
+  fc.tuple(
+    fc
+      .json()
+      .map((v) => String(v))
+      .filter((s) => s.length > 0),
+    fc.json()
+  )
+);
+
+const commands = [
+  fc.integer({ min: 0, max: tuples.length - 1 }).map((index) => {
+    const [k, v] = tuples[index];
+
+    return new AddCommand(k, v);
+  }),
+
+  fc.integer({ min: 0, max: tuples.length - 1 }).map((index) => {
+    const [k] = tuples[index];
+
+    return new RemoveCommand(k);
+  }),
+
+  fc.integer({ min: 0, max: tuples.length - 1 }).map((index) => {
+    const [k] = tuples[index];
+
+    return new LookupCommand(k);
+  }),
+];
 
 export { commands };
--- src/06-hash-table/with-fast-check/index.test.ts
+++ src/06-hash-table/with-fast-check/index.test.ts
@@ -1,82 +1,22 @@
 import fc from "fast-check";
 
-import {
-  circularQueueFactory,
-  circularQueueArrayFactory,
-  circularQueueObjectFactory,
-} from "..";
+import { hashTableFactory } from "..";
 
 import { commands, Model } from "./commands";
 
 describe.each`
-  name        | factory
-  ${"Map"}    | ${circularQueueFactory}
-  ${"Array"}  | ${circularQueueArrayFactory}
-  ${"Object"} | ${circularQueueObjectFactory}
-`(
-  "-> $name implementation of circular queue with fast-check",
-  ({ factory, name }) => {
-    /**
-     * Array implementation requires upper bound on size of queue, otherwise
-     * we get heap memory allocation error
-     */
-    const maxSize = new RegExp(name, "i").test("array") ? 100 : undefined;
+  name           | factory
+  ${"HashTable"} | ${hashTableFactory}
+`("-> $name with fast-check", ({ factory, name }) => {
+  test("-> model-based", () => {
+    fc.assert(
+      fc.property(fc.commands(commands, 100), (cmds) => {
+        const hashTable = factory();
+        const model: Model = {};
+        const sut = () => ({ model, real: hashTable });
 
-    test("-> model-based", () => {
-      fc.assert(
-        fc.property(
-          fc.integer({ min: 0, max: maxSize }),
-          fc.commands(commands, 100),
-          (size, cmds) => {
-            const queue = factory(size);
-            const model: Model = { writes: 0, size, values: [] };
-            const sut = () => ({ model, real: queue });
-
-            fc.modelRun(sut, cmds);
-          }
-        )
-      );
-    });
-
-    test("-> always reads from beginning of queue", () => {
-      fc.assert(
-        fc.property(
-          fc.integer({ min: 0, max: maxSize }),
-          fc.array(fc.json()),
-          (size, xs) => {
-            const queue = factory(size);
-            const boundXs = xs.slice(0, size);
-
-            boundXs.map((x) => queue.enqueue(x));
-            boundXs.map((x) => {
-              expect(queue.dequeue()).toEqual(x);
-            });
-          }
-        )
-      );
-    });
-
-    test("-> only writes to next value if number of unread writes is less than size", () => {
-      fc.assert(
-        fc.property(
-          fc.integer({ min: 0, max: maxSize }),
-          fc.array(fc.json()),
-          (size, xs) => {
-            const queue = factory(size);
-            let writes = 0;
-
-            xs.map((x) => {
-              const result = queue.enqueue(x);
-
-              if (result !== null) {
-                writes = writes + 1;
-              }
-            });
-
-            expect(writes).toBeLessThanOrEqual(size);
-          }
-        )
-      );
-    });
-  }
-);
+        fc.modelRun(sut, cmds);
+      })
+    );
+  });
+});
