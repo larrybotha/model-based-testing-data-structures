eea83dd64bcefe3717a116299bff18ac61466b22
Larry Botha <larry@fixate.it>
2021-01-27 21:45:38 +0200

    add linked list methods

--- src/06-linked-list/index.ts
+++ src/06-linked-list/index.ts
@@ -1,11 +1,25 @@
 export interface LinkedList<T = any> {
   add(value: T): void;
+  remove(value: T): void;
+  isEmpty(): boolean;
+  indexOf(value: T): number;
+  elementAt(index: number): T;
 }
 
 function linkedListFactory<T = any>(): LinkedList<T> {
   function add(value: T) {}
+  function remove(value: T) {}
+  function isEmpty() {
+    return true;
+  }
+  function indexOf(value: T) {
+    return -1;
+  }
+  function elementAt(index: number) {
+    return (undefined as unknown) as T;
+  }
 
-  return { add };
+  return { add, isEmpty, remove, indexOf, elementAt };
 }
 
 export { linkedListFactory };
