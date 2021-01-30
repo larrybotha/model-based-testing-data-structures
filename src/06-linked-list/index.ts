0eb8fe372f3cacafcc74f0b1a9cfba1e8c15dda0
Larry Botha <larry@fixate.it>
2021-01-30 23:26:26 +0200

    WIP - implement linked list with tests

--- src/06-linked-list/index.ts
+++ src/06-linked-list/index.ts
@@ -1,6 +1,10 @@
-interface LinkedListNode<T = any> {
-  element: T;
-  next: LinkedListNode<T>;
+interface LinkedListNode<Value = any> {
+  element: Value;
+  next: LinkedListNode<Value> | null;
+}
+
+interface LinkedListNodeFactory<Value = any> {
+  (value: Value): LinkedListNode<Value>;
 }
 
 export interface LinkedList<T = any> {
@@ -9,25 +13,90 @@ export interface LinkedList<T = any> {
   isEmpty(): boolean;
   indexOf(value: T): number;
   elementAt(index: number): T;
-  removeAt(index: number): LinkedListNode<T> | null;
+  removeAt(index: number): void;
   addAt(index: number, value: T): boolean;
 }
 
+const linkedListNodeFactory: LinkedListNodeFactory = (value) => ({
+  element: value,
+  next: null,
+});
+
 function linkedListFactory<T = any>(): LinkedList<T> {
-  function add(value: T) {}
-  function remove(value: T) {}
-  function isEmpty() {
-    return true;
+  let prefixNode = linkedListNodeFactory(null);
+  let length: number = 0;
+
+  function add(value: T) {
+    const node = linkedListNodeFactory(value);
+    let currNode = prefixNode;
+
+    while (currNode.next !== null) {
+      currNode = currNode.next;
+    }
+
+    currNode.next = node;
+
+    length++;
   }
+
+  function remove(value: T) {
+    let currNode = prefixNode;
+    let found = false;
+
+    while (currNode.next !== null && !found) {
+      if (currNode.next.element === value) {
+        currNode.next = currNode.next.next;
+        found = true;
+        length--;
+      } else {
+        currNode = currNode.next;
+      }
+    }
+  }
+
   function indexOf(value: T) {
-    return -1;
+    let currNode = prefixNode.next;
+    let index = 0;
+
+    while (currNode && currNode.element !== value) {
+      currNode = currNode.next;
+      index++;
+    }
+
+    return index < length ? index : -1;
   }
+
+  function isEmpty() {
+    return length === 0;
+  }
+
   function elementAt(index: number) {
-    return (undefined as unknown) as T;
+    let currNode = prefixNode.next;
+    let currIndex = 0;
+
+    while (currNode && currIndex < index) {
+      currNode = currNode.next;
+      currIndex++;
+    }
+
+    return currNode ? currNode.element : null;
   }
 
   function removeAt(index: number) {
-    return null;
+    let currNode = prefixNode;
+    let currIndex = -1;
+
+    while (currNode.next && currIndex < Math.max(length, index)) {
+      currNode = currNode.next;
+      currIndex++;
+    }
+
+    console.log(currNode);
+
+    if (currNode && currIndex === index) {
+      currNode.next = currNode.next ? currNode.next.next : null;
+      length--;
+    }
   }
 
   function addAt(index: number, value: T) {
--- src/06-linked-list/with-fast-check/commands.ts
+++ src/06-linked-list/with-fast-check/commands.ts
@@ -4,7 +4,7 @@ import { LinkedList } from "..";
 
 type LinkedListCommand = Command<any, LinkedList>;
 
-export type Model = amy[];
+export type Model = any[];
 
 /**
  * AddCommand
@@ -16,7 +16,10 @@ class AddCommand implements LinkedListCommand {
 
   check = () => true;
 
-  run = (m: Model, r: LinkedList) => {};
+  run = (m: Model, r: LinkedList) => {
+    m.push(this.value);
+    r.add(this.value);
+  };
 
   toString = () => `add(${this.value})`;
 }
@@ -31,11 +34,140 @@ class RemoveCommand implements LinkedListCommand {
 
   check = () => true;
 
-  run = (m: Model, r: LinkedList) => {};
+  run = (m: Model, r: LinkedList) => {
+    const mIndex = m.indexOf(this.value);
+
+    if (mIndex > -1) {
+      for (let i = mIndex + 1; i < m.length; i++) {
+        m[i - 1] = m[i];
+      }
+
+      m.pop();
+    }
+
+    r.remove(this.value);
+  };
 
   toString = () => `remove(${this.value})`;
 }
 
-const commands = [];
+/**
+ * IndexOfCommand.
+ *
+ * @implements {LinkedListCommand}
+ */
+class IndexOfCommand implements LinkedListCommand {
+  constructor(private value: any) {}
+
+  check = () => true;
+
+  run = (m: Model, r: LinkedList) => {
+    const mIndex = m.indexOf(this.value);
+    const rIndex = r.indexOf(this.value);
+
+    expect(rIndex).toBe(mIndex);
+  };
+
+  toString = () => `indexOf(${this.value})`;
+}
+
+/**
+ * IsEmptyCommand.
+ *
+ * @implements {LinkedListCommand}
+ */
+class IsEmptyCommand implements LinkedListCommand {
+  constructor() {}
+
+  check = () => true;
+
+  run = (m: Model, r: LinkedList) => {
+    expect(r.isEmpty()).toBe(m.length === 0);
+  };
+
+  toString = () => `isEmpty()`;
+}
+
+/**
+ * ElementAtCommand.
+ *
+ * @implements {LinkedListCommand}
+ */
+class ElementAtCommand implements LinkedListCommand {
+  constructor(private index: number) {}
+
+  check = () => true;
+
+  run = (m: Model, r: LinkedList) => {
+    const mValue = m[this.index] || null;
+    const rValue = r.elementAt(this.index);
+
+    expect(rValue).toBe(mValue);
+  };
+
+  toString = () => `elementAt(${this.index})`;
+}
+
+/**
+ * RemoveAtCommand.
+ *
+ * @implements {LinkedListCommand}
+ */
+class RemoveAtCommand implements LinkedListCommand {
+  constructor(private index: number) {}
+
+  check = () => true;
+
+  run = (m: Model, r: LinkedList) => {
+    if (this.index > -1 && m.length > this.index) {
+      for (let i = this.index + 1; i < m.length; i++) {
+        m[i - 1] = m[i];
+      }
+
+      m.pop();
+    }
+
+    r.removeAt(this.index);
+  };
+
+  toString = () => `removeAt(${this.index})`;
+}
+
+const tuples = fc.sample(fc.tuple(fc.integer({ min: 0 }), fc.json()));
+const arbTupleIndex = fc.integer({ min: 0, max: tuples.length - 1 });
+
+const commands = [
+  arbTupleIndex.map((index) => {
+    const [, value] = tuples[index];
+
+    return new AddCommand(value);
+  }),
+
+  arbTupleIndex.map((index) => {
+    const [, value] = tuples[index];
+
+    return new RemoveCommand(value);
+  }),
+
+  arbTupleIndex.map((index) => {
+    const [, value] = tuples[index];
+
+    return new IndexOfCommand(value);
+  }),
+
+  arbTupleIndex.map((index) => {
+    const [i] = tuples[index];
+
+    return new ElementAtCommand(i);
+  }),
+
+  arbTupleIndex.map((index) => {
+    const [i] = tuples[index];
+
+    return new RemoveAtCommand(i);
+  }),
+
+  fc.constant(new IsEmptyCommand()),
+];
 
 export { commands };
