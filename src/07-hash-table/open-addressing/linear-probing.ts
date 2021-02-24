f3a0f9548aaaeb9c670248745d88c4c09c66cab2
Larry Botha <larry@fixate.it>
2021-02-24 22:43:58 +0200

    refactor linear probing

--- src/07-hash-table/open-addressing/linear-probing.ts
+++ src/07-hash-table/open-addressing/linear-probing.ts
@@ -2,6 +2,8 @@ import { HashTable, HashTableKey, HashingFunction } from "../types";
 
 type Entry<Value = any> = [HashTableKey, Value];
 
+const log = 0 ? console.log.bind(console) : () => {};
+
 function hashTableLinearProbingFactory<T = any>(): HashTable<T> {
   let collection: Array<Entry<T> | undefined> = [];
   let size = 10;
@@ -37,6 +39,8 @@ function hashTableLinearProbingFactory<T = any>(): HashTable<T> {
     let count = 0;
     let added = false;
 
+    log("add before", collection, key, value);
+
     while (count < size) {
       const currHashedKey = hash(key, count);
       const valueAtHashedKey = collection[currHashedKey];
@@ -51,6 +55,7 @@ function hashTableLinearProbingFactory<T = any>(): HashTable<T> {
           length++;
         }
 
+        log("add after", collection, key, value);
         break;
       }
 
@@ -72,6 +77,9 @@ function hashTableLinearProbingFactory<T = any>(): HashTable<T> {
    */
   const remove: HashTable["remove"] = function remove(key) {
     let count = 0;
+    let removed = false;
+
+    log("remove before", collection, key);
 
     while (count < size) {
       const currHashedKey = hash(key, count);
@@ -81,16 +89,39 @@ function hashTableLinearProbingFactory<T = any>(): HashTable<T> {
       if (keyAtIndex === key) {
         collection[currHashedKey] = undefined;
         length--;
+        removed = true;
         break;
       }
 
       count++;
+    }
+
+    if (removed && count === 0) {
+      let hasNextKeys = true;
+
+      while (hasNextKeys) {
+        const nextHashedKey = hash(key, count + 1);
+        const valueAtHashedKey = collection[nextHashedKey];
+        const [keyAtIndex] = deconstruct(valueAtHashedKey);
+
+        if (keyAtIndex && `${keyAtIndex}`.length === `${key}`.length) {
+          collection[nextHashedKey - 1] = collection[nextHashedKey];
+        } else {
+          hasNextKeys = false;
+        }
+      }
+    }
+
+    if (removed) {
+      log("remove after", collection, key);
     }
   };
 
   const lookup: HashTable["lookup"] = function lookup(key) {
     const { length: arrLength } = collection;
     let result;
+
+    log("lookup", collection, key);
 
     for (let i = 0; i < arrLength; i++) {
       const currHashedKey = hash(key, i);
