7ba94cb0ec51c9b3093e553f9657ed39ff6521e0
Larry Botha <larry@fixate.it>
2021-03-14 23:06:51 +0200

    WIP - doubly linked list

--- src/08-doubly-linked-list/index.ts
+++ src/08-doubly-linked-list/index.ts
@@ -1,12 +1,53 @@
-import { DoublyLinkedList } from "./types";
+import { DoublyLinkedList, DoublyLinkedListNode } from "./types";
+
+function createNode<Data = any>(
+  data: Data,
+  previous: DoublyLinkedListNode<Data>["previous"] = null
+): DoublyLinkedListNode<Data> {
+  return {
+    data,
+    previous,
+    next: null,
+  };
+}
 
 function doublyLinkedListFactory<Value = any>(): DoublyLinkedList<Value> {
+  let tail: DoublyLinkedListNode | null = null;
+  let head: DoublyLinkedListNode | null = null;
+
   function add(value: Value) {
-    console.log(value);
+    const node = createNode(value);
+
+    if (!head) {
+      head = node;
+    }
+
+    if (tail) {
+      tail.next = node;
+    }
+
+    tail = node;
+  }
+
+  function remove(value: Value) {
+    let prevNode = createNode(null);
+    prevNode.next = head;
+
+    while (prevNode.next) {
+      const currNode = prevNode.next;
+      const { data, next } = currNode;
+
+      if (data === value) {
+        prevNode.next = next;
+      }
+
+      prevNode = currNode;
+    }
   }
 
   return {
     add,
+    remove,
   };
 }
 
--- src/08-doubly-linked-list/types.ts
+++ src/08-doubly-linked-list/types.ts
@@ -1,3 +1,10 @@
+export interface DoublyLinkedListNode<Data = any> {
+  data: Data;
+  previous: DoublyLinkedListNode | null;
+  next: DoublyLinkedListNode | null;
+}
+
 export interface DoublyLinkedList<Value = any> {
   add(value: Value): void;
+  remove(value: Value): void;
 }
