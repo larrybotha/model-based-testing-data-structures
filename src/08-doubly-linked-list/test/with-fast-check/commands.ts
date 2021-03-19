682bde37a20ada87f8f93465d680ccffb1432cfe
Larry Botha <larry@fixate.it>
2021-03-19 23:55:03 +0200

    update tests

--- src/08-doubly-linked-list/test/with-fast-check/commands.ts
+++ src/08-doubly-linked-list/test/with-fast-check/commands.ts
@@ -4,24 +4,80 @@ import { DoublyLinkedList } from "../../types";
 
 type DoublyLinkedListCommand = Command<any, DoublyLinkedList>;
 
-export interface Model {
-  [key: string]: any;
-}
+export type Model = any[];
 
 /**
  * AddCommand
  *
- * @implements {HashTableCommand}
+ * @implements {DoublyLinkedListCommand}
  */
-class AddCommand implements HashTableCommand {
+class AddCommand implements DoublyLinkedListCommand {
   constructor(public value: any) {}
 
   check = () => true;
 
-  run = (m: Model, r: HashTable) => {};
+  run = (m: Model, r: DoublyLinkedList) => {
+    m.push(this.value);
+
+    r.add(this.value);
+  };
 
   toString = () => `add(${this.value})`;
 }
-const commands = [];
+
+/**
+ * RemoveCommand
+ *
+ * @implements {DoublyLinkedListCommand}
+ */
+class RemoveCommand implements DoublyLinkedListCommand {
+  constructor(public value: any) {}
+
+  check = () => true;
+
+  run = (m: Model, r: DoublyLinkedList) => {
+    const index = m.indexOf(this.value);
+    const tmp = m.filter((_, i) => i !== index);
+
+    for (let i = 0; i < m.length; i++) {
+      m[i] = tmp[i];
+    }
+
+    m.pop();
+
+    r.remove(this.value);
+  };
+
+  toString = () => `remove(${this.value})`;
+}
+
+/**
+ * ElementAtCommand
+ *
+ * @implements {DoublyLinkedListCommand}
+ */
+class ElementAtCommand implements DoublyLinkedListCommand {
+  constructor(public index: number) {}
+
+  check = () => true;
+
+  run = (m: Model, r: DoublyLinkedList) => {
+    const modelValue = this.index >= m.length ? null : m[this.index];
+    const realValue = r.elementAt(this.index);
+
+    expect(realValue).toEqual(modelValue);
+  };
+
+  toString = () => `elementAt(${this.index})`;
+}
+
+const values = fc.sample(fc.json(), 100);
+const arbIndex = fc.integer({ min: 0, max: values.length });
+
+const commands = [
+  arbIndex.map((index) => new AddCommand(values[index])),
+  arbIndex.map((index) => new RemoveCommand(values[index])),
+  arbIndex.map((index) => new ElementAtCommand(index)),
+];
 
 export { commands };
--- src/08-doubly-linked-list/test/with-fast-check/index.test.ts
+++ src/08-doubly-linked-list/test/with-fast-check/index.test.ts
@@ -1,26 +1,18 @@
 import fc from "fast-check";
 
-import {
-  hashTableDoubleHashFactory,
-  hashTableLinearProbingFactory,
-  hashTableQuadraticProbingFactory,
-  hashTableLinkedListFactory,
-} from "../..";
+import { doublyLinkedListFactory } from "../..";
 
 import { commands, Model } from "./commands";
 
 describe.each`
-  name                                                | factory
-  ${"HashTable - Open Addressing: Double Hashing"}    | ${hashTableDoubleHashFactory}
-  ${"HashTable - Open Addressing: Linear Probing"}    | ${hashTableLinearProbingFactory}
-  ${"HashTable - Open Addressing: Quadratic Probing"} | ${hashTableQuadraticProbingFactory}
-  ${"HashTable - Separate Chaining: Linked List"}     | ${hashTableLinkedListFactory}
+  name                    | factory
+  ${"Doubly Linked List"} | ${doublyLinkedListFactory}
 `("-> $name with fast-check", ({ factory }) => {
   test("-> model-based", () => {
     fc.assert(
       fc.property(fc.commands(commands, 100), (cmds) => {
         const hashTable = factory();
-        const model: Model = {};
+        const model: Model = [];
         const sut = () => ({ model, real: hashTable });
 
         fc.modelRun(sut, cmds);
