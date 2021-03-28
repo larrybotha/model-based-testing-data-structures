0cff8bbff9a65a972d2e2c9eb8210dff1d0d7506
Larry Botha <larry@fixate.it>
2021-03-28 22:27:51 +0200

    begin types for bst

---
+++ src/09-binary-search-tree/types.ts
@@ -0,0 +1,10 @@
+export interface BinarySearchTreeNode<Value = any> {
+  value: Value;
+  left: BinarySearchTreeNode | null;
+  right: BinarySearchTreeNode | null;
+}
+
+export interface BinarySearchTree<Value = any> {
+  add(value: Value): void;
+  isPresent(value: Value): boolean;
+}
